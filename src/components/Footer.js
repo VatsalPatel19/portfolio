45b40a6bbeaa2e8d19b838f23b1406cbc2aeb0c3
Vatsal Kamlesh Patel <55306198+VatsalPatel19@users.noreply.github.com>
2024-05-07 16:40:58 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,19 @@
-import React from 'react'
-import '../components/Footer.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-export const Footer = () => {
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import '../components/Footer.css';
 
+const Footer = () => {
   return (
-    <>
-      {/* row */}
-      <div className='container-fluid row footer text-center'>
-        <div className='footericons'>
-          <a href="https://www.instagram.com/__vatsalpatel__/" target="blank"><FontAwesomeIcon icon={faInstagram} /></a>
-          <a href="https://www.facebook.com/vatsal.patel.1042" target="blank"><FontAwesomeIcon icon={faFacebook} /></a>
-          <a href="https://www.linkedin.com/in/vatsal-kamlesh-patel-615801190/" target="blank"><FontAwesomeIcon icon={faLinkedin} /></a>
-          <a href="https://github.com/VatsalPatel19" target="blank"><FontAwesomeIcon icon={faGithub} /></a></div>
+    <div className='container-fluid row footer text-center'>
+      <div className='footericons'>
+        <a href="https://www.instagram.com/__vatsalpatel__/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
+        <a href="https://www.facebook.com/vatsal.patel.1042" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} /></a>
+        <a href="https://www.linkedin.com/in/vatsal-kamlesh-patel-615801190/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+        <a href="https://github.com/VatsalPatel19" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
       </div>
-    </>
-  )
+    </div>
+  );
 }
+
+export default Footer;
