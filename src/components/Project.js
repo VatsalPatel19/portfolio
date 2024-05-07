a907014ad1f30d49469a86b10b3f0c060e56341c
Vatsal Kamlesh Patel <55306198+VatsalPatel19@users.noreply.github.com>
2024-05-07 16:44:48 -0400

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import './Project.css'
-import gujarat from '../images/gujarat.png'
-import cardtwo from '../images/stego.jpg'
-import cardone from '../images/port.png'
+import React from 'react';
+import './Project.css';
+import gujarat from '../images/gujarat.png';
+import cardtwo from '../images/stego.jpg';
+import cardone from '../images/port.png';
 
-export const Project = () => {
-
+const Project = () => {
   return (
     <div className='container-fluid'>
       {/* first row */}
@@ -16,7 +15,8 @@ export const Project = () => {
         <div className="card">
           <img src={cardone} alt="Avatar" />
           <div className="container">
-            <p className='text-center'>Personal Portfolio</p><p>This website is developed using ReactJs.</p>
+            <p className='text-center'>Personal Portfolio</p>
+            <p>This website is developed using ReactJs.</p>
             <p className='text-center'><a href='/'><button type="button">View Project</button></a></p>
           </div>
         </div>
@@ -26,9 +26,8 @@ export const Project = () => {
           <img src={cardtwo} alt="Avatar" />
           <div className="container">
             <p className='text-center'>Secure Stego</p>
-            <p>Secure Image Steganographic software in which encryption is done
-              using Blowfish Algorithm and Transmission of Image is done using Apache Kafka.</p>
-            <p className='text-center'><a href='https://ieeexplore.ieee.org/document/9716292' target="blank"><button type="button">View Project</button></a></p>
+            <p>Secure Image Steganographic software in which encryption is done using Blowfish Algorithm and Transmission of Image is done using Apache Kafka.</p>
+            <p className='text-center'><a href='https://ieeexplore.ieee.org/document/9716292' target="_blank" rel="noopener noreferrer"><button type="button">View Project</button></a></p>
           </div>
         </div>
 
@@ -38,12 +37,12 @@ export const Project = () => {
           <div className="container">
             <p className='text-center'>Gujarat Tourism Website</p>
             <p>Developed using HTML, CSS, PHP in my 2nd year of B.E.</p>
-            <p className='text-center'><a href='https://gujarattourism65.000webhostapp.com/index.html' target="blank"><button type="button">View Project</button></a></p>
+            <p className='text-center'><a href='https://gujarattourism65.000webhostapp.com/index.html' target="_blank" rel="noopener noreferrer"><button type="button">View Project</button></a></p>
           </div>
         </div>
       </div>
-
-
     </div>
-  )
+  );
 }
+
+export default Project;
